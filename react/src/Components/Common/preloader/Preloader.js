545f7bed290c352bab07b8da0ef8829315e094c6
Dima <pylypenkodima1@gmail.com>
2024-02-04 21:57:43 +0100

    update info

--- react/src/Components/Common/preloader/Preloader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import preLoader from '../../../assets/img/loading.gif';
-
-let Preloader = (props) => {
-  return  <img style={{ width: 100, height: 100 }}  src={ preLoader }/>
-}
-export  default Preloader;
